e25d6dc6319b594aaf58f9d11db5edc524a2c960
Pedro Henrique de Melo Ladeia <pedro.ladeia02@gmail.com>
2023-12-21 23:08:09 -0300

    :construction: feat: Added the find all method

--- src/main/java/com/aiblog/aiblog/controllers/PostController.java
+++ src/main/java/com/aiblog/aiblog/controllers/PostController.java
@@ -1,15 +1,27 @@
 package com.aiblog.aiblog.controllers;
 
+import com.aiblog.aiblog.models.PostModel;
+import com.aiblog.aiblog.services.PostService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping()
-public class TesteController {
+import java.util.List;
 
-    @GetMapping("/posts")
-    public String teste() {
-        return "End point funcionando com sucesso";
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RequestMapping("/post")
+public class PostController {
+    final PostService postService;
+    public PostController(PostService postService) { // Injection point
+        this.postService = postService;
+    }
+    @GetMapping("")
+    public ResponseEntity<List<PostModel>> findAll() {
+        return ResponseEntity.status(HttpStatus.OK).body(postService.findAll());
     }
 }
