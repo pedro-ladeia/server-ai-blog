042784c8e05965b3b397bf631262237ed7a66cdb
Pedro Henrique de Melo Ladeia <pedro.ladeia02@gmail.com>
2023-12-26 17:22:27 -0300

    :construction: feat: Created post method

--- src/main/java/com/aiblog/aiblog/services/PostService.java
+++ src/main/java/com/aiblog/aiblog/services/PostService.java
@@ -4,6 +4,8 @@ import com.aiblog.aiblog.models.PostModel;
 import com.aiblog.aiblog.repositories.PostRepository;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.List;
 
 @Service
@@ -15,5 +17,11 @@ public class PostService {
 
     public List<PostModel> findAll() { // Return all registers
         return postRepository.findAll();
+    }
+
+    public PostModel save(PostModel postModel) {
+        postModel.setCreatedAt(LocalDateTime.now(ZoneId.of("UTC")));
+        postModel.setUpdatedAt(LocalDateTime.now(ZoneId.of("UTC")));
+        return postRepository.save(postModel);
     }
 }
