9544d1f4dfee8f810b08d793d9c07e6bd59a67f4
Pedro Henrique de Melo Ladeia <pedro.ladeia02@gmail.com>
2024-01-08 15:49:16 -0300

    :sparkles: feat: Created Delete by id method

--- src/main/java/com/aiblog/aiblog/services/PostService.java
+++ src/main/java/com/aiblog/aiblog/services/PostService.java
@@ -2,11 +2,14 @@ package com.aiblog.aiblog.services;
 
 import com.aiblog.aiblog.models.PostModel;
 import com.aiblog.aiblog.repositories.PostRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class PostService {
@@ -19,9 +22,20 @@ public class PostService {
         return postRepository.findAll();
     }
 
+    public Optional<PostModel> findById(UUID id) {
+        return postRepository.findById(id);
+    }
+
     public PostModel save(PostModel postModel) {
         postModel.setCreatedAt(LocalDateTime.now(ZoneId.of("UTC")));
         postModel.setUpdatedAt(LocalDateTime.now(ZoneId.of("UTC")));
         return postRepository.save(postModel);
+    }
+    public boolean existsById(UUID id) {
+        return postRepository.existsById(id);
+    }
+    @Transactional
+    public void delete (UUID id) {
+        postRepository.deleteById(id);
     }
 }
